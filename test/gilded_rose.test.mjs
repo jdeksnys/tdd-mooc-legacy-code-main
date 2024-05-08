e4641e0b331296d682fe216c007141628fcbcc38
jdeksnys <jonasdeksnys@gamil.com>
2024-05-08 23:41:23 +0300

    test name:Backstage...

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -9,4 +9,11 @@ describe("Gilded Rose", () => {
     let res = JSON.stringify(items[0]);
     expect(res).to.equal(`{"name":"foo","sellIn":-1,"quality":0}`);
   });
+
+  test("name: Backstage...", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    let res = JSON.stringify(items[0]);
+    expect(res).to.equal(`{"name":"Backstage passes to a TAFKAL80ETC concert","sellIn":-1,"quality":0}`);
+  });
 });
