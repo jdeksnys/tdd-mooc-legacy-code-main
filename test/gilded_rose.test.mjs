25fb248d1f023849250f07d7c7dd268fa99b4298
jdeksnys <jonasdeksnys@gamil.com>
2024-05-09 13:48:03 +0300

    test-pass

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -217,24 +217,25 @@ describe("Gilded Rose", () => {
     let res = JSON.stringify(items[0]);
     expect(res).to.equal(`{"name":"Sulfuras, Hand of Ragnaros","sellIn":0,"quality":55}`);
   });
-  test("name: Sulfuras, Hand of Ragnaros; sellIn=0; quality<10", () => {
-    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 9)]);
+  test("name: Sulfuras, Hand of Ragnaros; sellIn<10; quality=0", () => {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 9, 0)]);
     const items = gildedRose.updateQuality();
     let res = JSON.stringify(items[0]);
-    expect(res).to.equal(`{"name":"Sulfuras, Hand of Ragnaros","sellIn":0,"quality":9}`);
+    expect(res).to.equal(`{"name":"Sulfuras, Hand of Ragnaros","sellIn":9,"quality":0}`);
   });
-  test("name: Sulfuras, Hand of Ragnaros; sellIn=0; quality>10", () => {
-    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 33)]);
+  test("name: Sulfuras, Hand of Ragnaros; sellIn>10; quality=0", () => {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 33, 0)]);
     const items = gildedRose.updateQuality();
     let res = JSON.stringify(items[0]);
-    expect(res).to.equal(`{"name":"Sulfuras, Hand of Ragnaros","sellIn":0,"quality":33}`);
+    expect(res).to.equal(`{"name":"Sulfuras, Hand of Ragnaros","sellIn":33,"quality":0}`);
   });
-  test("name: Sulfuras, Hand of Ragnaros; sellIn=0; quality>50", () => {
-    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 55)]);
+  test("name: Sulfuras, Hand of Ragnaros; sellIn>50; quality=0", () => {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 55, 0)]);
     const items = gildedRose.updateQuality();
     let res = JSON.stringify(items[0]);
-    expect(res).to.equal(`{"name":"Sulfuras, Hand of Ragnaros","sellIn":0,"quality":55}`);
+    expect(res).to.equal(`{"name":"Sulfuras, Hand of Ragnaros","sellIn":55,"quality":0}`);
   });
+
 
   test("name: Backstage passes to a TAFKAL80ETC concert; sellIn<0", () => {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", -10, 10)]);
