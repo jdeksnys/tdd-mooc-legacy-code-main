ac9c45766698d0137b3600a8e35d9a81d4c0b5ca
jdeksnys <jonasdeksnys@gamil.com>
2024-05-09 13:22:25 +0300

    test-pass

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -23,20 +23,31 @@ describe("Gilded Rose", () => {
     let res = JSON.stringify(items[0]);
     expect(res).to.equal(`{"name":"foo","sellIn":-11,"quality":8}`);
   });
-
   test("name: foo; quality<10", () => {
     const gildedRose = new Shop([new Item("foo", 10, -10)]);
     const items = gildedRose.updateQuality();
     let res = JSON.stringify(items[0]);
     expect(res).to.equal(`{"name":"foo","sellIn":9,"quality":-10}`);
   });
-
   test("name: foo; quality<10", () => {
     const gildedRose = new Shop([new Item("foo", -10, -10)]);
     const items = gildedRose.updateQuality();
     let res = JSON.stringify(items[0]);
     expect(res).to.equal(`{"name":"foo","sellIn":-11,"quality":-10}`);
   });
+  test("name: foo; quality>50", () => {
+    const gildedRose = new Shop([new Item("foo", 10, 55)]);
+    const items = gildedRose.updateQuality();
+    let res = JSON.stringify(items[0]);
+    expect(res).to.equal(`{"name":"foo","sellIn":9,"quality":54}`);
+  });
+  test("name: foo; sellIn>50", () => {
+    const gildedRose = new Shop([new Item("foo", 55, 10)]);
+    const items = gildedRose.updateQuality();
+    let res = JSON.stringify(items[0]);
+    expect(res).to.equal(`{"name":"foo","sellIn":54,"quality":9}`);
+  });
+
 
   test("name: foo; sellIn>50; quality<10", () => {
     const gildedRose = new Shop([new Item("foo", 55, -10)]);
