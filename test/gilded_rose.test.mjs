d6dbeed88fe7bdb6bde6c42ceb12625721b40b97
jdeksnys <jonasdeksnys@gamil.com>
2024-05-10 12:50:18 +0300

    refactor. test-pass

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -109,12 +109,12 @@ describe("Gilded Rose", () => {
   });
 
 
-  // test("name: Backstage passes to a TAFKAL80ETC concert; quality<10", () => {
-  //   const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
-  //   const items = gildedRose.updateQuality();
-  //   let res = JSON.stringify(items[0]);
-  //   expect(res).to.equal(`{"name":"Backstage passes to a TAFKAL80ETC concert","sellIn":-1,"quality":0}`);
-  // });
+  test("name: Backstage passes to a TAFKAL80ETC concert; quality<10", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    let res = JSON.stringify(items[0]);
+    expect(res).to.equal(`{"name":"Backstage passes to a TAFKAL80ETC concert","sellIn":-1,"quality":0}`);
+  });
 
   // test("name: Backstage passes to a TAFKAL80ETC concert; sellIn>50", () => {
   //   const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 55, 10)]);
