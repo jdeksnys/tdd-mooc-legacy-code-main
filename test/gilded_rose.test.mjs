818d928bf04310ad20bbaeefb0a3a9a019fc1654
jdeksnys <jonasdeksnys@gamil.com>
2024-05-10 14:31:03 +0300

    Refactor. test-pass

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -87,6 +87,13 @@ describe("Gilded Rose", () => {
     expect(res).to.equal(`{"name":"Aged Brie","sellIn":9,"quality":50}`);
   });
 
+  test("name: Aged Brie; sellIn=0; quality=1", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 20, 10)]);
+    const items = gildedRose.updateQuality();
+    let res = JSON.stringify(items[0]);
+    expect(res).to.equal(`{"name":"Aged Brie","sellIn":19,"quality":11}`);
+  });
+
   test("name=Aged Brie; sellIn=10; quality=50", () => {
     const gildedRose = new Shop(["blablabla"]);
     let items;
